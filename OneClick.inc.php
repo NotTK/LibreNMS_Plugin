5bb93d4ae46831cd582331b7a018ba23f93da9e9
NotTK <90275404+NotTK@users.noreply.github.com>
2023-10-26 18:07:55 -0500

    Update OneClick.inc.php

--- OneClick.inc.php
+++ OneClick.inc.php
@@ -1,9 +1,29 @@
-<html> 
+<html>
+<head>
+  <title> Button Example </title>
+</head>
 <body>
+    <div class = "panel panel-default">
+     <div class = "pull-left">
+      <buttom id = "clickButton"> Click me </button>
+    </div>
   <div class = "panel panel-default">
     <div class = "pull-right"> 
     <img src = "plugins/OneClick/images/USLA.png" title = "USA LOGO" style = "max-height: 50px">
     </div>
   </div>
+
+        <script>
+          document.getElementById("clickButton").addEventListener("click", function() {
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "plugins/OneClick/main.py", true);
+            xhr.onreadystatechange = function() {
+              if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log("Python script executed successfully!");
+              }
+            };
+            xhr.send();
+          });
+        </script>
 </body>
 </html>
