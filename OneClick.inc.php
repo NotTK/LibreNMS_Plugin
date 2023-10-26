a767f22b0b8eff6d9b13b7826c92310abdfdde28
NotTK <90275404+NotTK@users.noreply.github.com>
2023-10-26 15:21:31 -0500

    Update OneClick.inc.php

--- OneClick.inc.php
+++ OneClick.inc.php
@@ -2,7 +2,7 @@
 <body>
   <div class = "panel panel-default">
     <div class = "pull-right"> 
-    <img src = "plugins/LibreNMS_Plugin/USAL.jpeg" title = "USA LOGO" style = "max-height: 50px">
+    <img src = "plugins/LibreNMS_Plugin/USLA.png" title = "USA LOGO" style = "max-height: 50px">
     </div>
   </div>
 </body>
