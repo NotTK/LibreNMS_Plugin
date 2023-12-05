e7a5c24dac2a5b436fe804a082c1478be56b1c03
NotTK <90275404+NotTK@users.noreply.github.com>
2023-12-05 15:04:50 -0600

    Update OneclickController.php

--- OneclickController.php
+++ OneclickController.php
@@ -1,1 +1,49 @@
+//cd /app/Https/Controllers/Device/Tabs/(filename)
+
+namespace App\Http\Controllers\Device\Tabs;
+
+use App\Models\Device;
+use Illuminate\Foundations\Auth\Access\AuthorizeRequests;
+use Illuminate\Http\Request;
+use LibreNMS\Interfaces\UI\DeviceTab;
+
+class OneclickController implemnets DeviceTab {
+
+  use AuthorizeRequests;
+
+  public function visible(Device $device): bool {
+    return true;
+  }
+
+  public function slug(): string {
+    return 'oneclick';
+  }
+
+  public function icon(): string {
+    return 'fa-file-text-o';
+  }
+
+  public function name(): string {
+    return __('Oneclick Diagnostics');
+  }
+
+  public function data(Device $device): array {
+    return [];
+  }
+
+  public function update (Request $request, Device $device) {
+    $this->authorize('update-notes', $device);
+
+    $device->notes = $request->input('note');
+    $device->save();
+  }
+}
+
+
+
+
+
+
+
+
 
