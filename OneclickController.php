e7e84dcbe4011474bbffca6ee8cd0e32833e4ce3
NotTK <90275404+NotTK@users.noreply.github.com>
2023-12-05 14:58:44 -0600

    Update OneclickController.php

--- OneclickController.php
+++ OneclickController.php
@@ -1,1 +1,28 @@
+//cd /resources/views/device/tabs/(filename)
+
+@extends('device.index')
+
+@section('tab')
+
+<html>
+  <head>
+    <title>Oneclick Diagnostics Button Below</title>
+  </head>
+
+  <body>
+
+    <?php
+      if (isset($_POST['myButton'])) {
+        echo "Button Clicked!";
+      }
+    ?>
+
+  <form method = "post" action = "{{ route('executePythonScript') }}">
+    @csrf
+    <buttont type = "submit" name = "myButton"> Click for Diagnostics </buttont>
+  </form>
+
+  </body>
+</html>
+@endsection
 
