7df63247b5b2957efa9ef2c3dc80781bb0d2163f
NotTK <90275404+NotTK@users.noreply.github.com>
2023-12-13 17:29:31 -0600

    Update oneclick.blade.php

--- oneclick.blade.php
+++ oneclick.blade.php
@@ -10,16 +10,39 @@
 
   <body>
 
-    <?php
-      if (isset($_POST['myButton'])) {
-        echo "Button Clicked!";
-      }
-    ?>
+    @if(isset($result))
+      <p>{{ $result }} </p>
+    @elseif(isset($error))
+      <p>{{ $error }} </p>
+    @endif
 
-  <form method = "post" action = "{{ route('executePythonScript') }}">
-    @csrf
-    <button type = "submit" name = "myButton"> Click for Diagnostics </buttont>
-  </form>
+    <form method = "post" action = "" id = "myForm">
+      @csrf
+      <button type = "submit" onclick = "handleButtonClick(event)"> Click for Diagnostics </button>
+    </form>
+
+    <div id = "result"> </div>
+
+    <script>
+        function handleButtonClick() {
+            event.preventDefault();
+            document.getElementById('myForm').insertAdjacentHTML('afterend', '<p>Button has been clicked</p>');
+            fetch('/run-python-script', {
+              method: 'POST',
+              headers: {
+                'Content-Type':'application/json',
+                'X-CSRF-TOKEN': document.querySelector('meta[name = "csrf-token"]').getAttribute('content'),
+              },
+              body: JSON.stringify({}),
+            })
+          .then(response => response.json())
+          .then(data => {
+            document.getElementById('result').innerHTML = '<p>' + data.output.join('<br>') + '</p>';
+          })
+          .catch(error => console.error('Error.', error));
+        }
+      
+    </script>
 
   </body>
 </html>
